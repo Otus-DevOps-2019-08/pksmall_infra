cde5c50d1f9ec7153f070a6c699bf1a94f4cafb1
Pavel Korzhenko <pksmall@live.ru>
2019-09-13 16:26:47 -0700

    Added new test task in test.py

--- play-travis/test.py
+++ play-travis/test.py
@@ -5,5 +5,8 @@ class NumbersTest(unittest.TestCase):
     def test_equal(self):
         self.assertEqual(1 + 1, 1)
 
+    def test_notequal(self):
+        self.assertEqual(1 + 2, 1)
+
 if __name__ == '__main__':
     unittest.main()
