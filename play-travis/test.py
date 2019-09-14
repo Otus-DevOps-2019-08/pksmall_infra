25efe54687298b3f4eacdf91b92f605ebc3602ee
Pavel Korzhenko <pksmall@live.ru>
2019-09-14 11:12:38 -0700

    edit test.py

--- play-travis/test.py
+++ play-travis/test.py
@@ -3,13 +3,13 @@ import unittest
 class NumbersTest(unittest.TestCase):
 
     def test_equal(self):
-        self.assertEqual(1 + 1, 1)
+        self.assertEqual(1 + 1, 2)
 
     def test_notequal(self):
-        self.assertNotEqual(2, 1)
+        self.assertNotEqual(1, 1)
 
     def test_gre(self):
-        self.assertGreater(1, 2)
+        self.assertGreater(2, 1)
 
 if __name__ == '__main__':
     unittest.main()
